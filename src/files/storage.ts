2cd29fccfe022209dd0e066d12cf60235e9d98fd
Nik68rus <nik200x@mail.ru>
2023-05-01 15:08:16 +0300

    pushing to git

---
+++ src/files/storage.ts
@@ -0,0 +1,22 @@
+import { Request } from 'express';
+import { diskStorage } from 'multer';
+
+const generateId = () =>
+  Array(18)
+    .fill(null)
+    .map(() => Math.round(Math.random() * 16).toString(16))
+    .join('');
+
+const normalizeFileName = (
+  req: Request,
+  file: Express.Multer.File,
+  callback: (error: Error | null, filename: string) => void,
+) => {
+  const fileExtName = file.originalname.split('.').pop();
+  callback(null, `${generateId()}.${fileExtName}`);
+};
+
+export const fileStorage = diskStorage({
+  destination: './uploads',
+  filename: normalizeFileName,
+});
---
+++ src/users/users.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from './entities/user.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(UserEntity) private repo: Repository<UserEntity>,
+  ) {}
+
+  async findByEmail(email: string) {
+    return this.repo.findOneBy({ email });
+  }
+
+  async findById(id: number) {
+    return this.repo.findOneBy({ id });
+  }
+
+  create(createUserDto: CreateUserDto) {
+    const user = this.repo.create(createUserDto);
+    return this.repo.save(user);
+  }
+}
